4808435afb7df760e050811fa3f6cecc8efe5b95
yossaj <yossaj@icloud.com>
2019-04-09 19:43:10 +0100

    MVP

--- spec/river_spec.rb
+++ spec/river_spec.rb
@@ -7,12 +7,18 @@ require_relative("../bear.rb")
 class TestRiver < MiniTest::Test
 
   def setup
-
+    @fish = Fish.new('salmon')
     @river = River.new('Amazon',['salmon','salmon','pike','catfish'])
+    @bear = Bear.new("Yogi", "Grizzly",[])
   end
 
   def test_river_is_amazon
     assert_equal('Amazon', @river.name)
+  end
+
+  def test_river_loses_fish
+    @bear.eat_fish_from_river(@river)
+    assert_equal(3,@river.fish.count)
   end
 
 end
